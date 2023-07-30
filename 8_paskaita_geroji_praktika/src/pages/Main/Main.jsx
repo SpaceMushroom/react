793941c122c834cc5b1e10abb172c682a0d16732
Ignas Grinius <igrinius@gmail.com>
2023-07-30 16:43:37 +0300

    small update

--- 8_paskaita_geroji_praktika/src/pages/Main/Main.jsx
+++ 8_paskaita_geroji_praktika/src/pages/Main/Main.jsx
@@ -1,7 +1,18 @@
+import Skills from "../../components/Skills/Skills";
 import "./Main.scss";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit incidunt
+        maxime temporibus culpa id magnam dolorum laboriosam. Harum nisi
+        dignissimos molestias magni a rerum commodi cupiditate modi vero! Quod,
+        itaque?
+      </div>
+      <Skills />
+    </div>
+  );
 };
 
 export default Main;
